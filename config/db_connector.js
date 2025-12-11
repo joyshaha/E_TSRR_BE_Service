022ff59889367a8cd94de08c0f26d7c8bcfa05fb
joyshaha <eee.joy.ckz@gmail.com>
2025-12-11 11:43:07 +0600

    restructure

--- config/db_connector.js
+++ config/db_connector.js
@@ -1,18 +1,19 @@
 const connectDB = require("./mongo");
-const sequelize = require("./pg");
+const getSequelize = require("../models/index");
 
 const connectDatabase = async () => {
   try {
     // Connect to MongoDB
     connectDB();
+    console.log('MongoDB connected');
     // Connect to Postgres
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
+    getSequelize()
+    console.log("Postgres connection successful");
     // Sync the database (Create tables if not exists)
     // await sequelize.sync({ alter: true });
     // console.log('Postgres tables synchronized successfully');
   } catch (error) {
-    console.error('Unable to connect to the postgres database:', error);
+    console.error('Unable to connect to database:', error);
     process.exit(1);
   }
 };
---
+++ models/index.js
@@ -0,0 +1,64 @@
+const { Sequelize } = require("sequelize");
+const { production, development } = require('../config/config');
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+let sequelize;
+
+function getSequelize() {
+  if (!sequelize) {
+    sequelize = new Sequelize(development.url, {
+      dialect: development.dialect,
+      dialectOptions: development.dialectOptions,
+    });
+  }
+  sequelize.authenticate();
+  return sequelize;
+}
+
+// // Option 1: Passing parameters separately (other dialects)
+// // const path = require('path');
+// // const fs = require('fs');
+
+// // const sequelize = new Sequelize(
+// //     process.env.DB_NAME,
+// //     process.env.DB_USER,
+// //     process.env.DB_PASSWORD,
+// //     {
+// //       host: process.env.DB_HOST,
+// //       port: process.env.DB_PORT,
+// //       dialect: 'postgres',
+// //       dialectOptions: {
+// //         ssl: {
+// //           require: true,
+// //           rejectUnauthorized: false,
+// //         },
+// //       },
+// //       logging: process.env.NODE_ENV === 'development' ? console.log : false,
+// //     }
+// // );
+
+// // Option 2: Passing a connection URI
+// // const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+// //   dialect: "postgres",
+// //   dialectOptions: {
+// //     ssl: {
+// //       require: true,
+// //       rejectUnauthorized: false,
+// //       ca: process.env.PG_CA_CERT || undefined
+// //     }
+// //   }
+// // });
+// const sequelize = new Sequelize(
+//   process.env.NODE_ENV === 'production' ? production.url : development.url, 
+//   {
+//       dialect: process.env.NODE_ENV === 'production' ? production.dialect : development.dialect,
+//       dialectOptions: process.env.NODE_ENV === 'production' ? production.dialectOptions : development.dialectOptions,
+//       logging: process.env.NODE_ENV === 'development' ? console.log : false,
+//     }
+// )
+
+// module.exports = sequelize;
+
+module.exports = getSequelize;
