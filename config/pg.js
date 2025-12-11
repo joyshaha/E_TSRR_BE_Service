f9060845202889bf5ed024b441a078d3c3a3a573
joyshaha <eee.joy.ckz@gmail.com>
2025-12-11 10:16:51 +0600

    update for check db

--- config/pg.js
+++ config/pg.js
@@ -1,23 +1,10 @@
 const { Sequelize } = require('sequelize');
-const { development } = require('./config');
+const { production, development } = require('./config');
 
 const dotenv = require('dotenv');
 dotenv.config();
 
 // Option 1: Passing parameters separately (other dialects)
-// const sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: process.env.DB_HOST,
-//       port: process.env.DB_PORT,
-//       dialect: 'postgres',
-//       logging: process.env.NODE_ENV === 'development' ? console.log : false,
-//     }
-// );
-
-// Option 2: Passing a connection URI
 // const path = require('path');
 // const fs = require('fs');
 
@@ -39,14 +26,7 @@ dotenv.config();
 //     }
 // );
 
-
-const sequelize = new Sequelize(development.url, {
-  dialect: development.dialect,
-  dialectOptions: development.dialectOptions,
-  logging: process.env.NODE_ENV === 'development' ? console.log : false,
-});
-
-
+// Option 2: Passing a connection URI
 // const sequelize = new Sequelize(process.env.POSTGRES_URL, {
 //   dialect: "postgres",
 //   dialectOptions: {
@@ -57,5 +37,13 @@ const sequelize = new Sequelize(development.url, {
 //     }
 //   }
 // });
+const sequelize = new Sequelize(
+  process.env.NODE_ENV === 'production' ? production.url : development.url, 
+  {
+      dialect: process.env.NODE_ENV === 'production' ? production.dialect : development.dialect,
+      dialectOptions: process.env.NODE_ENV === 'production' ? production.dialectOptions : development.dialectOptions,
+      logging: process.env.NODE_ENV === 'development' ? console.log : false,
+    }
+)
 
 module.exports = sequelize;
